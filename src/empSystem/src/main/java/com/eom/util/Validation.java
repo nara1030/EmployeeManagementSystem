920d0611398c13d432496d181da4b87e73f01f77
Hong-Jun Eom <nara1030@naver.com>
2020-01-31 17:04:10 +0900

    유효성 검사 클래스 추가

--- src/empSystem/src/main/java/com/eom/util/Validation.java
+++ src/empSystem/src/main/java/com/eom/util/Validation.java
@@ -1,4 +1,22 @@
 package com.eom.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Validation {
+    // Employee 클래스: Setter/Getter
+    // Validation 클래스: 입력 유효성 검사(Employee 객체 생성 전)
+    public static boolean checkPhoneNumFormat(String phoneNum) {
+        Pattern pattern = Pattern.compile("^[0-9]{2,3}-[0,9]{3,4}-[0,9]{4}$");
+        Matcher matcher = pattern.matcher(phoneNum);
+
+        return matcher.find();
+    }
+
+    public static boolean checkEmailFormat(String email) {
+        Pattern pattern = Pattern.compile("(^[0-9a-zA-Z_-]+@[0-9a-zA-Z]+[.][a-zA-Z]{2,4}$)");
+        Matcher matcher = pattern.matcher(email);
+
+        return matcher.find();
+    }
 }
